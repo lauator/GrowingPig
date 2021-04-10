f367421b4a4a03eceaaf7df28df346432a4d8bd8
Lautaro Casanueva <casanuevalautaro@gmail.com>
2021-04-10 17:47:38 -0300

    primer commit, primera parte UI completa

---
+++ app/src/main/java/com/example/growingpig/model/Task.kt
@@ -0,0 +1,9 @@
+package com.example.growingpig.model
+
+class Task {
+    var title = ""
+    var priority = ""
+    var checkBox = false
+
+
+}
---
+++ app/src/main/java/com/example/growingpig/view/ui/activities/SignUpActivity.kt
@@ -0,0 +1,34 @@
+package com.example.growingpig.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.growingpig.R
+
+
+class SignUpActivity : AppCompatActivity() {
+    /*
+     TODO  En principio voy a hacer un Shared Preferences para guardar los datos del usuario, a pesar de que se
+     de que no es la mejor forma de guardar datos sensibles, pero como la app no guarda informacion tan importante
+     es tolerable
+     */
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+
+
+
+
+
+
+
+
+
+}
