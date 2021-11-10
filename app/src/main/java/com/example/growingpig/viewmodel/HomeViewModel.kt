90de10e7eb37d96f6819dd0c1490bd65a326b85e
Lautaro Casanueva <casanuevalautaro@gmail.com>
2021-11-10 13:29:58 -0300

    Un par de detalles

--- app/src/main/java/com/example/growingpig/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/growingpig/viewmodel/HomeViewModel.kt
@@ -11,7 +11,7 @@ import com.example.growingpig.model.repository.database.AppDatabase
 class HomeViewModel(app: Application) : AndroidViewModel(app) {
 
     var allTasks: MutableLiveData<List<Task>> = MutableLiveData()
-
+    val taskDao = AppDatabase.getAppDatabase((getApplication())).taskDao()
 
     init {
         getAllTasks()
@@ -22,33 +22,33 @@ class HomeViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     fun getAllTasks() {
-        val taskDao = AppDatabase.getAppDatabase((getApplication())).taskDao()
+
         val list = taskDao.getAllTasks()
 
         allTasks.postValue(list)
     }
 
     fun createTask(task: Task) {
-        val taskDao = AppDatabase.getAppDatabase(getApplication()).taskDao()
+
         taskDao.createTask(task)
         getAllTasks()
     }
 
     fun updateTask(task: Task) {
-        val taskDao = AppDatabase.getAppDatabase(getApplication()).taskDao()
+
         taskDao.updateTask(task)
         getAllTasks()
     }
 
     fun deleteTask(task: Task) {
-        val taskDao = AppDatabase.getAppDatabase(getApplication()).taskDao()
+
         taskDao.deleteTask(task)
         getAllTasks()
 
     }
 
     fun deleteAllTasks() {
-        val taskDao = AppDatabase.getAppDatabase(getApplication()).taskDao()
+
         taskDao.deleteAllTasks()
         getAllTasks()
     }
